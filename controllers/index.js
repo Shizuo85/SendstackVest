540b88de89cc2b4c7facd018a110f503cebfeea8
Ekene <ekeneanthony85@gmail.com>
2023-11-16 13:53:41 +0100

    Eleventh commit

--- controllers/index.js
+++ controllers/index.js
@@ -1,126 +1,77 @@
 exports.splitPayment = (req, res, next) => {
     let { ID, Amount, Currency, CustomerEmail, SplitInfo } = req.body;
+    SplitInfo = SplitInfo.sort((x, y) => {
+        const a = x.SplitType.toUpperCase(),
+            b = y.SplitType.toUpperCase();
+        return a == b ? 0 : a > b ? 1 : -1;
+    });
 
-    let SplitBreakdown = [];
+    const SplitBreakdown = [];
+    let ratioTotal = 0;
+    const arrayLength = SplitInfo.length;
+    Amount = parseInt(Amount);
 
-    Amount = parseFloat(Amount);
-
-    if (SplitInfo.length < 1 || SplitInfo.length > 20) {
+    if (arrayLength < 1 || arrayLength > 20) {
         return next(
-            new Error(
-                "The SplitInfo array can only contain 1-20 entities"
-            )
+            new Error("The SplitInfo array can only contain 1-20 entities")
         );
     }
 
-    let trackingObj = {
-        Amount: Amount,
-        postFlat: Amount,
-        postPercent: Amount,
-        ratioSum: 0,
-        remPercentAccumulator: 1,
-        length: SplitInfo.length
-    };
+    const ratioArray = SplitInfo.filter((SplitInfo) => {
+        const splitType = SplitInfo.SplitType.toLowerCase();
+        const splitValue = parseInt(SplitInfo.SplitValue);
 
-    getValue(0, SplitInfo, trackingObj, SplitBreakdown);
+        if (splitType == "flat") {
+            Amount -= splitValue;
+            SplitBreakdown.push({
+                SplitEntityId: SplitInfo["SplitEntityId"],
+                Amount: splitValue,
+            });
 
+            if (Amount < 0) {
+                return next(
+                    new Error(
+                        "Excessive Flat Split Value, final Balance cannot be less than 0"
+                    )
+                );
+            }
+        } else if (splitType == "percentage") {
+            if (splitValue > 100) {
+                return next(
+                    new Error(
+                        "Excessive percentage Split Value, final Balance cannot be less than 0"
+                    )
+                );
+            }
+            const x = (splitValue / 100) * Amount;
+            SplitBreakdown.push({
+                SplitEntityId: SplitInfo["SplitEntityId"],
+                Amount: x,
+            });
+
+            Amount -= x;
+        } else {
+            ratioTotal += splitValue;
+            return true;
+        }
+        return false;
+    });
+    
+    if (ratioArray.length != 0) {
+        ratioArray.every((SplitInfo) => {
+            const x = (parseInt(SplitInfo.SplitValue) / ratioTotal) * Amount;
+            SplitBreakdown.push({
+                SplitEntityId: SplitInfo["SplitEntityId"],
+                Amount: x,
+            });
+            return true
+        });
+        Amount = 0;
+    }
+    
     res.status(200).json({
         ID,
-        Balance: trackingObj.Amount,
-        SplitBreakdown
+        Balance: Amount,
+        SplitBreakdown,
     });
 };
-
-function getValue(index, arr, trackingObj, SplitBreakdown) {
-    let x = arr[index];
-
-    let ownPercent = 1;
-
-    let splitType = x.SplitType.toLowerCase();
-    let splitValue = parseFloat(x.SplitValue);
-    switch (splitType) {
-        case "flat": {
-            trackingObj.postFlat -= splitValue;
-
-            if (trackingObj.postFlat >= 0) {
-                trackingObj.Amount -= splitValue;
-                SplitBreakdown.push({
-                    SplitEntityId: x["SplitEntityId"],
-                    Amount: splitValue,
-                });
-            } else {
-                trackingObj.postFlat += splitValue;
-                SplitBreakdown.push({
-                    SplitEntityId: x["SplitEntityId"],
-                    Amount: 0,
-                });
-            }
-            break;
-        }
-
-        case "percentage": {
-            ownPercent = splitValue / 100 * trackingObj.remPercentAccumulator;
-            trackingObj.remPercentAccumulator *= (100 - splitValue) / 100;
-            break;
-        }
-
-        case "ratio": {
-            trackingObj.ratioSum += splitValue;
-            break;
-        }
-    }
-
-    index += 1;
-    if (index == trackingObj.length) {
-        trackingObj.postPercent = trackingObj.remPercentAccumulator * trackingObj.postFlat;
-    }
-    else {
-        getValue(index, arr, trackingObj, SplitBreakdown);
-    }
-
-    
-
-    switch (splitType) {
-        case "flat": {
-            return;
-        }
-
-        case "percentage": {
-            let value = ownPercent * trackingObj.postFlat;
-            if (value > 0){
-                trackingObj.Amount -= value;
-                SplitBreakdown.push({
-                    SplitEntityId: x["SplitEntityId"],
-                    Amount: value,
-                });
-            }
-            else{
-                SplitBreakdown.push({
-                    SplitEntityId: x["SplitEntityId"],
-                    Amount: 0,
-                });
-            }
-            break;
-        }
-
-        case "ratio": {
-            let value = splitValue / trackingObj.ratioSum * trackingObj.postPercent;
-            if (value > 0){
-                trackingObj.Amount -= value;
-                SplitBreakdown.push({
-                    SplitEntityId: x["SplitEntityId"],
-                    Amount: value,
-                });
-            }
-            else{
-                SplitBreakdown.push({
-                    SplitEntityId: x["SplitEntityId"],
-                    Amount: 0,
-                });
-            }
-            break;
-        }
-    }
-
-    return;
-}
